234779a3a6503cb996e3fbe961c4804908727f5d
Arthur <arthurferreia1417@gmail.com>
2025-06-05 23:40:38 -0300

    STYLE: finalizando estilo da home

--- SafeZone/obj/Debug/net8.0/SafeZone.AssemblyInfo.cs
+++ SafeZone/obj/Debug/net8.0/SafeZone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SafeZone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f38eecb663fa676644cff5a2aee5996391c6090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82cc17180afd9393abb5c6b6756d780df9f224f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SafeZone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SafeZone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
